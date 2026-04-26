bdd9868e411d54bd06d1837f72d39862f6ce520d
zaib-dev22 <zaib.link.22@gmail.com>
2026-04-26 11:22:32 +0500

    changes

--- src/components/Home/Community/Community.jsx
+++ src/components/Home/Community/Community.jsx
@@ -3,9 +3,9 @@ import Cards from './Cards.jsx'
 
 const Community = (props) => {
   return (
-    <div className="h-[75] flex flex-col items-center justify-around">
+    <div className="h-[75] flex flex-col ">
       <Text/>
-    <div className='flex items-center m-20 mb-5 justify-between'> {props.cards.map(function(elem,idx){
+    <div className='flex m-20 mb-5 '> {props.cards.map(function(elem,idx){
 
 return <Cards key={idx}  img={elem.img} heading={elem.Heading1} heading2={elem.Heading2}/>
       })}
